51b7bc68e13daa28657db07ef27b248430619a01
Salman <salzahir18@gmail.com>
2025-04-21 16:18:21 -0500

    Fix login handler to update session and render index with user

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,7 @@
 const db = require('../db/queries');
 
+// This function handles the login process
+// It checks the username and password against the database
 async function handleLogin(req, res) { 
     const {username, password} = req.body;
 
@@ -8,6 +10,11 @@ async function handleLogin(req, res) {
     const user = await db.getLoginUser(username, password);
     if (user) {
         req.session.user = user;
+        console.log("User found:", user);
+        return res.render('index', {
+            user: req.session.user || null,
+            csrfToken: req.csrfToken()
+        });
     }
     else {
         res.status(401).send('Invalid username or password');
