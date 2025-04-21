9d4eb9a6347a9c8ba6c264ee4d349d608a82e13d
Salman <salzahir18@gmail.com>
2025-04-20 21:04:43 -0500

    Set up initial app.js with middleware, CSRF protection, session handling, and passport configuration.

---
+++ app.js
@@ -0,0 +1,45 @@
+// app.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Config 
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const session = require('express-session');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+
+app.use(cookieParser());
+app.use(session
+    ({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false } // Set to true if using HTTPS
+    })
+);
+app.use(csurf({ cookie: true }));
+
+const passport = require('passport');
+// const flash = require('connect-flash');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", (req, res, next) => {
+    res.send("Hello World");
+});
+
+module.exports = app;
