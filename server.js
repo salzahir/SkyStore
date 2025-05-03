2c5ee5acaa8904cdaea8039eaa85f21bae1db1a3
Salman <salzahir18@gmail.com>
2025-05-02 21:34:25 -0500

    Add environment-aware server startup log using devLog

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { app, PORT } from "./app.js";
+import { devLog } from "./src/utils/devlog.js";
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    devLog(`Server is running at http://localhost:${PORT} in ${process.env.NODE_ENV} mode`);
 });
